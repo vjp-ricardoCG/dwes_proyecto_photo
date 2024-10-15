4f3eaf07bd254f5b0a089c70f1e300e85d7a01d1
vjp-ricardoCG <rcaceresg05@informatica.iesvalledeljerteplasencia.es>
2024-10-15 20:37:20 +0200

    clase

--- contact.php
+++ contact.php
@@ -6,23 +6,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   
 
     $firstName = $_POST['FirstName'] ;
-   $lastName = $_POST['LastName'] ;
- $email = $_POST['Email'] ;
-   $subject = $_POST['Subject'] ;
-   $message = $_POST['Message'] ;
+    $lastName = $_POST['LastName'] ;
+    $email = $_POST['Email'] ;
+    $subject = $_POST['Subject'] ;
+    $message = $_POST['Message'] ;
 
 
-   if (  empty($firstName) ){
+    if (  empty($firstName) ){
      $arrayErrores[0]="Falta el campo FirstName";
-   }
-   if (  empty($email) ){
+    }
+    if (  empty($email) ){
     $arrayErrores[2]="Falta el campo Email";
-  }
-  if (  empty($subject) ){
+    }
+    if (  empty($subject) ){
     $arrayErrores[3]="Falta el campo Subject";
-  }
+    }
     $arrayCampos[0]=$firstName;
-   $arrayCampos[1]=$lastName;
+    $arrayCampos[1]=$lastName;
     $arrayCampos[2]=$email;
     $arrayCampos[3]=$subject;
     $arrayCampos[4]=$message;
