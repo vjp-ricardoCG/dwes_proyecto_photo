756f6977700507b0dee19da3c475ea702ef2cca7
vjp-ricardoCG <rcaceresg05@informatica.iesvalledeljerteplasencia.es>
2024-10-29 20:52:49 +0100

    clase 29/10/2024

---
+++ exceptions/FileException.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class FileException extends Exception {
+
+    public function __contruct(string $mensaje){
+        parent ::__construct( $mensaje );
+
+    }
+
+
+}
+
+
+
+?>
--- galeria.php
+++ galeria.php
@@ -1,11 +1,26 @@
 <?php
 
+require 'entities/File.class.php';
 $errores = [];
 $descripcion = '';
 $mensaje='';
 
 if ($_SERVER['REQUEST_METHOD'] ==='POST'){
-    $descripcion = trim(htmlspecialchars($_post['descripcion']));
+
+
+    try{
+
+    
+    $descripcion = trim(htmlspecialchars($_POST['descripcion']));
+
+    $tiposAceptados =['image/jpeg',"image/jpg","image/gif","image/png"];
+
+    $imagen= new File('imagen',$tiposAceptados);
     $mensaje = 'Datos enviados';
+
+    }catch(FileException $exception){
+
+        $errores[]=$exception->getMessage();
+    }
 }
 require 'views/galeria.view.php';
